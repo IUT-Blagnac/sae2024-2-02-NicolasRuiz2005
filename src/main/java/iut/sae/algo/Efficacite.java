98fd091b35022230de2f8fbd033d1a3d4ed2e031
Nicolas Ruiz <nicolas.ruiz2075@gmail.com>
2024-06-11 14:53:21 +0200

    fin algo efficacite

--- src/main/java/iut/sae/algo/Efficacite.java
+++ src/main/java/iut/sae/algo/Efficacite.java
@@ -1,44 +1,40 @@
 package iut.sae.algo;
 
 
-public class Simplicite{
-
+public class Efficacite{
 
     /* Méthode RLE simplicité
      * 
      * Avec un String builder, plutôt que de concaténer des chaînes de caractères
      */
-    public static String RLE(String in) {
-        // On vérifie si la chaîne d'entrée est vide
-        if (in == null || in.isEmpty()) {
+    public static String RLE(String input) {
+        if (input == null || input.isEmpty()) {
             return "";
         }
-
-        StringBuilder chaineFinale = new StringBuilder();
-        int cpt = 1;
-
-        // On parcourt la chaîne d'entrée en partant de la deuxième lettre
-        for (int i = 1; i < in.length(); i++) {
-            // Si le caractère actuel est le même que le précédent on augemente le compteur
-            if (in.charAt(i) == in.charAt(i - 1)) {
-                cpt++;
+    
+        char[] chars = input.toCharArray();
+        StringBuilder result = new StringBuilder();
+        int count = 1;
+    
+        for (int i = 1; i < chars.length; i++) {
+            if (chars[i] == chars[i - 1]) {
+                if (count == 9) {
+                    result.append(9).append(chars[i - 1]);
+                    count = 1;
+                } else {
+                    count++;
+                }
             } else {
-                // On ajoute le caractère et le nombre de répétitions à la chaîne finale
-                chaineFinale.append(cpt);
-                chaineFinale.append(in.charAt(i - 1));
-                // On réinitialise le compteur
-                cpt = 1;
+                result.append(count).append(chars[i - 1]);
+                count = 1;
             }
         }
-
-        // On ajoute le dernier groupe de caractères car la boucle s'arrête avant
-        chaineFinale.append(cpt);
-        chaineFinale.append(in.charAt(in.length() - 1));
-        
-        // Et on retourne la chaîne finale
-        return chaineFinale.toString();
+    
+        result.append(count).append(chars[chars.length - 1]);
+        return result.toString();
     }
     
+
 
     public static String RLE(String in, int iteration) throws AlgoException {
         // On vérifie si la chaîne d'entrée est vide
@@ -59,47 +55,27 @@ public class Simplicite{
         return in;
     }
 
-    
 
     public static String unRLE(String in) throws AlgoException {
-        // On vérifie si la chaîne d'entrée est vide ou nulle
         if (in == null || in.isEmpty()) {
             return "";
         }
-
-        StringBuilder chaineFinale = new StringBuilder(); // Chaîne finale
-        StringBuilder chaineNombre = new StringBuilder(); // Chaîne de nombre
-        int cpt = 0; // Compteur de répétitions
-
-
-        // On parcourt la chaîne d'entrée
-        for (int i = 0; i < in.length(); i++) {
-            char actuel = in.charAt(i); // Caractère actuel
-
-            // On regarde si le caractère actuel est un chiffre et si oui on l'ajoute à la chaîne de nombre
-            if (Character.isDigit(actuel)) {
-                chaineNombre.append(actuel);
-            } else {
-                // Si la chaîne de nombre n'est pas vide
-                if (chaineNombre.length() > 0) {
-                    // On convertit la chaîne de nombre en entier (le nombre de répétitions)
-                    cpt = Integer.parseInt(chaineNombre.toString());
-
-                    // On ajoute le caractère (cpt) fois à la chaîne finale
-                    for (int j = 0; j < cpt; j++) {
-                        chaineFinale.append(actuel);
-                    }
-
-                    // On réinitialise le compteur et la chaîne de nombre
-                    cpt = 0;
-                    chaineNombre.setLength(0);
+    
+        char[] chars = in.toCharArray();
+        StringBuilder chaineFinale = new StringBuilder();
+        int cpt = 0;
+    
+        for (int i = 0; i < chars.length; i++) {
+            if (Character.isDigit(chars[i])) {
+                cpt = chars[i] - '0';
+                for (int j = 0; j < cpt; j++) {
+                    chaineFinale.append(chars[i + 1]);
                 }
+                i++; // Passer au caractère suivant
             }
         }
-
-        // On retourne la chaîne finale
+    
         return chaineFinale.toString();
-
     }
 
 
