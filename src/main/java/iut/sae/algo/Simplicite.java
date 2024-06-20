f0200e8946f0425c7ed359ebd74f504f907ecf4b
Nicolas Ruiz <nicolas.ruiz2075@gmail.com>
2024-06-20 15:52:45 +0200

    fin SAE Depot rapports

--- src/main/java/iut/sae/algo/Simplicite.java
+++ src/main/java/iut/sae/algo/Simplicite.java
@@ -2,104 +2,63 @@ package iut.sae.algo;
 
 public class Simplicite {
     
-    /* Méthode RLE simplicité
-     * 
-     */
     public static String RLE(String in) {
-        // On vérifie si la chaîne d'entrée est vide ou null
-        if (in == null || in.isEmpty()) {
-            return "";
+    StringBuilder rle = new StringBuilder();
+    int count = 1;
+    for (int i = 0; i < in.length(); i++) {
+        if (i + 1 < in.length() && in.charAt(i) == in.charAt(i + 1) && count < 9) {
+            count++;
+        } else {
+            rle.append(count).append(in.charAt(i));
+            count = 1;
         }
-    
-        // Initialiser les variables
-        char currentChar = in.charAt(0); 
-        int cpt = 1;
-        String result = "";
-    
-        // Parcourir le reste de la chaîne 
-        for (int i = 1; i < in.length(); i++) {
-            char c = in.charAt(i);
-            if (c == currentChar) {
-                // Même caractère, on incrémente le compteur
-                cpt++;
-                if (cpt == 10) {
-                    // Limite de 9 atteinte, on écrit le bloc de 9
-                    result += "9" + currentChar;
-                    cpt = 1;
-                }
-            } else {
-                // Caractère différent, on écrit le bloc précédent
-                result += cpt + "" + currentChar;
-                currentChar = c;
-                cpt = 1;
-            }
-        }
-    
-        // Écrire le dernier bloc
-        result += cpt + "" + currentChar;
-        return result;
-    }
-    
-
-
-    public static String RLE(String in, int iterations) throws AlgoException {
-        // Vérifier les cas de base
-        if (in == null || in.isEmpty()) {
-            return "";
-        }
-    
-        // Vérifier la validité des itérations
-        if (iterations < 1) {
-            throw new AlgoException("Le nombre d'itérations doit être supérieur à 0");
-        }
-    
-        // Appliquer l'algorithme RLE autant de fois que le nombre d'itérations
-        String result = in;
-        for (int i = 0; i < iterations; i++) {
-            result = RLE(result);
-        }
-    
-        return result;
     }
 
+    return rle.toString();
+}
 
+
+
+
+
+    
+
+    public static String RLE(String in, int iteration) throws AlgoException{
+        String resultat=in;
+        for(int i=0;i<iteration;i++){
+            resultat=RLE(in);
+            in=resultat;
+
+        }
+        return resultat;
+    }
 
     public static String unRLE(String in) throws AlgoException {
-    // Vérifier les cas de base
-    if (in == null || in.isEmpty()) {
-        return "";
-    }
-
-    // Initialiser les variables
-    char[] strTab = in.toCharArray(); // Convertir la chaîne en tableau de caractères
-    String result = "";
-    int cpt = 0;
-
-    // Parcourir la chaîne d'entrée
-    for (int i = 0; i < strTab.length; i++) {
-        char c = strTab[i]; // Caractère courant
-        if (Character.isDigit(c)) { // Vérifier si le caractère est un chiffre
-            // Nombre de répétitions
-            cpt = c - '0'; 
-            i++; // Passer au caractère suivant
-            char repeatChar = strTab[i];
-            // Ajouter le caractère répété
-            for (int j = 0; j < cpt; j++) {
-                result += repeatChar;
+        StringBuilder resultat = new StringBuilder();
+        int tailleIn = in.length();
+        int nbBoucle;
+        for (int i = 0; i < tailleIn; i++) {
+            nbBoucle = Integer.parseInt("" + in.charAt(i));
+            i++;
+            char caractere = in.charAt(i);
+            for (int j = 0; j < nbBoucle; j++) {
+                resultat.append(caractere);
             }
         }
+        
+        return resultat.toString();
     }
+    
+    
+    public static String unRLE(String in, int iteration) throws AlgoException{
+        String resultat=in;
+        for(int i=0;i<iteration;i++){
+            resultat=unRLE(in);
+            in=resultat;
 
-        return result;
-    }
-
-    public static String unRLE(String in, int iteration) throws AlgoException {
-        // Appliquer l'algorithme unRLE autant de fois que le nombre d'itérations
-        for (int i = 0; i < iteration; i++) {
-            in = unRLE(in);
         }
+        return resultat; 
 
-        return in;
     }
 
 }
